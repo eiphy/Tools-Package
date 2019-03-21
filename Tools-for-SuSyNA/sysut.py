fde484726d92c9f8436b9ae977eb4ba6926a168c
eiphy <eiphnix@gmail.com>
2019-03-21 10:47:41 +0800

    Modity the repository structure & comment

--- Tools-for-SuSyNA/sysut.py
+++ Tools-for-SuSyNA/sysut.py
@@ -4,11 +4,14 @@ import del_event
 import stdize as std
 
 
+# For standardization
 def stdize(filer, filew):
     error = std.stdize(filer, filew)
 
+# For adding selfloop
 def selfloop(statec, eventc, filew, mode=0, filer='None'):
     error = add_self.add_self(statec, eventc, filew, mode, filer)
 
+# For delete event
 def delevent(eventc, filename):
     del_event.del_event(eventc, filename)
